abab6c244d1335482cd8468e66c7694041a9892a
Preston <preston@mycherrytree.com>
2021-06-09 00:35:44 -0600

    Updating junit-jest configuration to include path as suite name

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    reporters: ['default', ['jest-junit', { outputFile: 'junit.xml' }]],
+    reporters: ['default', ['jest-junit', { outputFile: 'junit.xml', usePathForSuiteName: 'true' }]],
     verbose: true,
 };
 
