6ebd289ce4fc1be02c833f4e972e906a8c7f48a6
Preston <preston@mycherrytree.com>
2021-06-09 00:31:08 -0600

    Switching Jest reporter to use a Junit variant for better visualization

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    reporters: ['default', ['jest-junit', { outputFile: 'junit.xml' }]],
     verbose: true,
 };
 
